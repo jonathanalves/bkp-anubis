e46a304f8d8eb9948508990986bbae9366e66608
Paulo <Paulo@DESKTOP-0BD8N56>
2017-09-25 14:22:38 -0300

    Passei alguns métodos do upload utils para o file utils

--- src/main/java/anubis/utils/FileUtils.java
+++ src/main/java/anubis/utils/FileUtils.java
@@ -1,10 +1,21 @@
 package anubis.utils;
 
+import java.awt.Image;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.SecureRandom;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import anubis.response.ResponseException;
 
@@ -59,6 +70,51 @@ public class FileUtils {
 	public static void saveFile(byte[] file, String pathName, String fileName) throws IOException {
         Path path = Paths.get(pathName + File.separator + fileName);
         Files.write(path, file);
+	}
+	
+	public static String generateCode() {
+		return new BigInteger(130, new SecureRandom()).toString(32) + DateUtils.getCalendar().getTimeInMillis();
+	}
+	
+	public static String upload(String path, String name, MultipartFile file) {
+
+		if (name.contains("/")) {
+			throw new ResponseException("utils.separador.diretorio.nao.permitido");
+		}
+
+		String fileName = DateUtils.getDiaMesAno(DateUtils.getCalendar()) + "-" + generateCode();
+
+		if (file.isEmpty()) {
+			throw new ResponseException("utils.arquivo.especifico.vazio", name);
+		}
+
+		try {
+			new File(path).mkdir();
+
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(path + FileUtils.SEPARATOR + fileName)));
+			FileCopyUtils.copy(file.getInputStream(), stream);
+			stream.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ResponseException(e, "utils.erro.enviar.arquivo", name);
+		}
+
+		return fileName;
+
+	}
+	
+	public static boolean isImagemValida(byte[] bytes) {
+		boolean valido = true;
+		try {
+		    Image image = ImageIO.read(new ByteArrayInputStream(bytes));
+		    if (image == null) {
+		        valido = false;
+		    }
+		} catch(IOException ex) {
+		    valido = false;
+		}
+		return valido;
 	}
 	
 }
